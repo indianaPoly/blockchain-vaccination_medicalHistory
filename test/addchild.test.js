830a9fa45208c7a62d763423f1035d7a1171e605
indianaPoly <95522176+indianaPoly@users.noreply.github.com>
2024-10-02 07:28:51 +0000

    test: 예방 접종 업데이트

--- test/addchild.test.js
+++ test/addchild.test.js
@@ -24,32 +24,44 @@ describe("Add children", () => {
     console.log("Test case executed.");
   });
 
-  it("should Add child", async () => {
+  it("should", async () => {
+    const printChildInfromation = async () =>  {
+      const childInformation = await vaacinationManager.returnChildInformation();
+
+      console.log("-------자식 정보 추출-------");
+      for (let index = 0; index < childInformation.length; index++) {
+        console.log("자식 주소 : ", childInformation[index].childAddress);
+        console.log("자식 이름 : ", childInformation[index].name);
+        console.log("자식 생년월일 : ", childInformation[index].birthDate);
+        console.log("자식 개월 수 : ", childInformation[index].babyMonth);
+  
+        console.log("자식 백신정보");
+        for (
+          let index2 = 0;
+          index2 < childInformation[index].vaccinations.length;
+          index2++
+        ) {
+          console.log(
+            `${index2 + 1}. `,
+            childInformation[index].vaccinations[index2]
+          );
+        }
+  
+        console.log("--------------------");
+      }
+    }
+
     await vaacinationManager.addChild("갑", 20240928);
     await vaacinationManager.addChild("을", 20240928);
     await vaacinationManager.addChild("병", 20240928);
-    const childInformation = await vaacinationManager.returnChildInformation();
 
-    console.log("-------자식 정보 추출-------");
-    for (let index = 0; index < childInformation.length; index++) {
-      console.log("자식 주소 : ", childInformation[index].childAddress);
-      console.log("자식 이름 : ", childInformation[index].name);
-      console.log("자식 생년월일 : ", childInformation[index].birthDate);
-      console.log("자식 개월 수 : ", childInformation[index].babyMonth);
+    await printChildInfromation();
 
-      console.log("자식 백신정보");
-      for (
-        let index2 = 0;
-        index2 < childInformation[index].vaccinations.length;
-        index2++
-      ) {
-        console.log(
-          `${index2 + 1}. `,
-          childInformation[index].vaccinations[index2]
-        );
-      }
+    // 만약 갑이 예방 접종을 맞는다고 하자.
+    console.log("갑이 백신을 맞습니다.");
+    await vaacinationManager.updateChildVaccination("갑", 'vaccine1');
+    console.log("갑의 백신예방 접종이 완료되었습니다.");
 
-      console.log("--------------------");
-    }
+    await printChildInfromation();
   });
 });
