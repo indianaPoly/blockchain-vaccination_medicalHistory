c9834d0276c32603790c8f9939e3cfa3f3d38fac
yujamint <woawoa0513@gmail.com>
2024-11-27 12:54:41 +0900

    feat: https 서버

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import e from "express";
 import cors from "cors";
+import fs from "fs";
+import https from "https";
 import childRoutes from "./routes/childRoutes.js";
 
 const app = e();
@@ -8,7 +10,26 @@ app.use(e.json());
 
 app.use("/contract", childRoutes);
 
-const PORT = 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const HTTP_PORT = 8080;
+const HTTPS_PORT = 8081;
+
+app.listen(HTTP_PORT, () => {
+  console.log(`Server is running on port ${HTTP_PORT}`);
 });
+
+// HTTPS server
+const domainName = process.env.DOMAIN_NAME;
+try {
+  const keyFile = fs.readFileSync(`/etc/letsencrypt/live/${domainName}/privkey.pem`);
+  const certFile = fs.readFileSync(`/etc/letsencrypt/live/${domainName}/fullchain.pem`);
+  const options = {
+    key: keyFile,
+    cert: certFile,
+  };
+
+  https.createServer(options, app).listen(HTTPS_PORT, () => {
+    console.log(`Server is running on port ${HTTPS_PORT}`);
+  });
+} catch (error) {
+  console.error(`There was a problem while running the server on HTTPS - message: ${error}`);
+}
